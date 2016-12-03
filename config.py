b89be987ec1c6a0df54bb52d786c8e3b7b0e131d
Green You <651992456@qq.com>
2016-12-03 16:07:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ SECRET_KEY = 'yoursecret'
 STRIPE_API_KEY = 'yourapisecret'
 BCRYPT_LEVEL = 13
 MAIL_FROM_EMAIL = "admin@example.com"
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@127.0.0.1:3306/store'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@server/fos'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SERVER_NAME = 'fos.dev'
